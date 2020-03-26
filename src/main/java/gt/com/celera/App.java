48cf78aef858608078674085cd59a5329b8e1998
Roberto C <chiroy_134@hotmail.com>
2020-03-26 15:27:38 -0600

    add bc provider to test layer

--- src/main/java/gt/com/celera/App.java
+++ src/main/java/gt/com/celera/App.java
@@ -1,13 +1,19 @@
 package gt.com.celera;
 
+import java.security.Security;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public String testBC(String name, Context context) {
+        System.out.println("Hello World!");
+        Security.addProvider(new BouncyCastleProvider());
+        return String.format("tutto bene ", name);
     }
 }
